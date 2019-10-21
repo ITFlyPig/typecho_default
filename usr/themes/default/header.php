9af67ce6d504fb0232dea63f7d2543a434416f7a
wangyuelin <a1659509224@163.com>
2019-10-21 14:51:25 +0800

    将分类标签添加到顶部分类栏目里面

--- usr/themes/default/header.php
+++ usr/themes/default/header.php
@@ -54,9 +54,9 @@
             <div class="col-mb-12">
                 <nav id="nav-menu" class="clearfix" role="navigation">
                     <a<?php if($this->is('index')): ?> class="current"<?php endif; ?> href="<?php $this->options->siteUrl(); ?>"><?php _e('首页'); ?></a>
-                    <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
-                    <?php while($pages->next()): ?>
-                    <a<?php if($this->is('page', $pages->slug)): ?> class="current"<?php endif; ?> href="<?php $pages->permalink(); ?>" title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
+                    <?php $this->widget('Widget_Metas_Category_List')->to($pages); ?>
+                    <?php while($array = $pages->next()): ?>
+                        <a<?php if($this->is('category', $pages->slug)): ?> class="current"<?php endif; ?> href="<?php $pages->permalink(); ?>" title="<?php echo $array['name'];?>"><?php echo $array['name'];?></a>
                     <?php endwhile; ?>
                 </nav>
             </div>
